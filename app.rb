34eb2e6de5a47960370960319ccc567991c4eafc
Vivian Allen <mrvivianallen@gmail.com>
2018-03-06 16:15:41 +0000

    cat route added

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ get '/Vivian and Noelle Rock!' do
   'Vivian and Noelle Rock!'
 end
 
-get '/secret' do
-  "Shhhh! It's BIG a secret"
+get '/cat' do
+  "<div style='border-style: double; border-color: red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
